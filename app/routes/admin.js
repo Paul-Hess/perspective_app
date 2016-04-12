cf8843c6e1f635ccd1d746814f9debaac25c97ee
Gracie Lundell <gracielundell@gmail.com>
2016-04-12 15:20:28 -0700

    delete admin ability to delete users, admin currently may only delete posts

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -3,13 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('post');
+    return this.store.findAll('user');
   },
   actions: {
     deletePost(post) {
       post.destroyRecord();
     }
-  },
-  deleteUser(user) {
-    user.destroyRecord();
   }
 });
