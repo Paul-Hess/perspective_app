ee7a1be388929be76a4d80f4b32cc14919ee9860
Gracie Lundell <gracielundell@gmail.com>
2016-04-12 10:11:18 -0700

    admin each loop working, posts deleting but model details not showing

--- app/models/post.js
+++ app/models/post.js
@@ -5,7 +5,7 @@ export default DS.Model.extend({
 	description: DS.attr(),
 	user: DS.belongsTo('user', {async: true}),
 	category: DS.belongsTo('category', {async: true}),
-	responses: DS.hasMany('response', {async: true}),
-	comments: DS.hasMany('comment', {async: true}),
-	voteTally: DS.attr()  
+	// responses: DS.hasMany('response', {async: true}),
+	// comments: DS.hasMany('comment', {async: true}),
+	voteTally: DS.attr()
 });
