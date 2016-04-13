4a856f00287993477066a679f899fbe02f05f863
Paul-Hess <pauljacksonhess@gmail.com>
2016-04-13 16:13:26 -0700

    add concurrent save of file system storage and json database records

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -5,13 +5,26 @@ export default Ember.Route.extend({
      return Ember.RSVP.hash({
       owner: false,
       currentUser: this.get('session').get('uid'),
-      user: this.store.findRecord('user', params.user_id)
+      user: this.store.findRecord('user', params.user_id),
+      post: this.store.findAll('post')
     }).then(function(model){
-      console.log(model.owner)
-      if(model.currentUser == model.user.id) {
-        model.owner = true
+      console.log(model.owner);
+      if(model.currentUser === model.user.id) {
+        model.owner = true;
       }
-      return model
+      return model;
     });
+  }, 
+
+  actions: {
+    savePost(params) {
+      var newPost = this.store.createRecord('post', params);
+      var user = params.user;
+      newPost.save().then(function() {
+        user.get('posts').addObject(newPost);
+        return user.save();
+      });
+    }
+
   }
 });
