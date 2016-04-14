33ae7d7f69237ca29fc1a7c07f155b661dd51764
Paul-Hess <pauljacksonhess@gmail.com>
2016-04-14 12:42:09 -0700

    add categories to posts

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,21 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('post');
-    return this.store.findAll('user');
+    return Ember.RSVP.hash({
+      post: this.store.findAll('post'),
+      user: this.store.findAll('user')
+    });
+
   },
   actions: {
     deletePost(post) {
       post.destroyRecord();
-    },
-
-    saveCategory(title) {
-    	var newCategory = this.store.createRecord('category', {
-    		title: title
-    	});
-    	if(newCategory.save()) {
-    		console.log('saved properly' );
-    	}
     }
   }
 });
