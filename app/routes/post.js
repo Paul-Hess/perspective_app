085e3f0250e710f898829903641e18ce7519b6b4
Paul-Hess <pauljacksonhess@gmail.com>
2016-04-14 14:57:54 -0700

    add responses to posts

--- app/routes/post.js
+++ app/routes/post.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('post', params.post_id);
+    return Ember.RSVP.hash({
+      user: this.store.findRecord('user', this.get('session').get('uid')),
+      post: this.store.findRecord('post', params.post_id)
+    })
   },
   actions: {
     saveReview3(params) {
@@ -10,6 +13,20 @@ export default Ember.Route.extend({
       var post = params.post;
       post.get('comments').addObject(newReview);
       newReview.save().then(function() {
+        return post.save();
+      });
+    },
+
+    saveResponse(params) {
+      var newResponse = this.store.createRecord('response', params);
+      var post = params.post;
+      var user = params.user;
+      console.log(params);
+
+      newResponse.save().then(function() {
+        user.get('responses').addObject(newResponse);
+        post.get('responses').addObject(newResponse);
+        user.save();
         return post.save();
       });
     }
