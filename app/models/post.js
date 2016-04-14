038c5fdb10a5ef5a2df3245f5d13a43b0debec98
David Remington <david.ian.remington@gmail.com>
2016-04-14 16:30:52 -0700

    add delete function to profile route

--- app/models/post.js
+++ app/models/post.js
@@ -7,8 +7,7 @@ export default DS.Model.extend({
 	medium: DS.attr(),
 	user: DS.belongsTo('user', {async: true}),
 	category: DS.attr(),
-	// need to cross out until this functionality is set up, right now it's breaking the code due to the hasMan having nothing. remember to add back into post.json file as well.
-	// responses: DS.hasMany('response', {async: true}),
+	responses: DS.hasMany('response', {async: true}),
 	comments: DS.hasMany('comment', {async: true}),
 	voteTally: DS.attr()
 });
--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
+import config from '../config/environment';
+import AWS from 'npm:aws-sdk';
 
 export default Ember.Route.extend({
   model(params) {
@@ -26,7 +28,37 @@ export default Ember.Route.extend({
       });
     },
     deletePost(post) {
-        
-      }
+      console.log("post object", post)
+        var self = this;
+        AWS.config.update({
+          accessKeyId: config.AWS_ACCESS_KEY_ID,
+          seretAccessKey: config.AWS_SECRET_ACCESS_KEY,
+          region: 'us-west-2'
+        });
+        var s3 = new AWS.S3();
+        console.log('post key', post.key)
+        var params = {
+          Bucket: 'reddit/images',
+          Key: post.key
+        };
+        s3.deleteObject(params, function(err,data){
+          if (err) {
+            console.log(err, err.stack);
+          } else {
+            console.log(data);
+          }
+        }).then(function(post) {
+            var response_deletions = post.get('responses').map(function(response) {
+              return response.destroyRecord();
+            });
+            var comment_deletions = post.get('comments').map(function(comment) {
+              return comment.destroyRecord();
+            });
+            Ember.RSVP.all(response_deletions, comment_deletions).then(function() {
+              return post.destroyRecord();
+              this.transitionTo('profile', params.user_id);
+            });
+         });
+    }
   }
 });
